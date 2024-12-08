53d2aa8ed5b17b1512e56e1b0ae7c06618f43a17
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2024-12-08 18:05:12 +0530

    Contact completed responsive has to be done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Contact from "./Component/Contact"
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">WELCOME MOTREN </h1>
+      
+      <Contact/>
     </>
   );
 }
